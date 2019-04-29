b425a0760c835e271defe2960f0cd3d11fe66ed2
Alexey Raspopov <oleksii.raspopov@gmail.com>
2019-04-29 00:41:18 -0400

    reimplemented the plugin with postcss parsing

--- index.js
+++ index.js
@@ -1,28 +1,52 @@
-module.exports = function StylesLiteralPlugin({ types, template }) {
+let postcss = require('postcss');
+let camelCase = require('camelcase');
+
+const TARGET_TAG_NAME = 'css';
+const EXPRESSION_PLACEHOLDER = '__EXPRESSION_PLACEHOLDER__';
+
+module.exports = function StylesLiteralPlugin({ types }) {
   return {
     name: 'styles-literal',
     visitor: {
       TaggedTemplateExpression(path) {
-        if (path.node.tag.name !== 'css') return;
-        let node = path.node;
-        let quasi = node.quasi;
-        let entries = [];
-        for (let q of quasi.quasis) {
-          let value = q.value.raw.trim();
-          let rules = value
-            .split(';')
-            .map(r => r.trim().replace(/\n/g, ' '))
-            .filter(r => r.length > 0);
-          for (let rule of rules) {
-            let [match, key, value] = rule.match(/^([^:]+)\:\s?([^$]+)$/);
-            let camelKey = key.replace(/\-([a-z])/g, (m, s) => s.toUpperCase());
-            entries.push([camelKey, value.replace(/\s+|\t+/g, ' ').trim()]);
-          }
-        }
-        let styleRules = entries.map(([k, v]) => `${k}: '${v}'`).join(',\n');
-        let result = template(`({ ${styleRules} })`);
-        path.replaceWith(result());
+        if (path.node.tag.name !== TARGET_TAG_NAME) return;
+
+        let { quasis, expressions } = path.node.quasi;
+        let css = quasis.map(v => v.value.raw).join(EXPRESSION_PLACEHOLDER);
+        let parsedCss = postcss.parse(css);
+        let declarations = parsedCss.nodes.filter(node => node.type === 'decl');
+
+        let entries = declarations.map(({ prop, value }) => {
+          let strings = value
+            .split(EXPRESSION_PLACEHOLDER)
+            .map(part => types.stringLiteral(part));
+
+          let joinedParts = join(strings, expressions).filter(
+            part => !types.isStringLiteral(part) || part.value.length > 0
+          );
+
+          return types.objectProperty(
+            types.identifier(camelCase(prop)),
+            joinedParts.reduce((acc, part) =>
+              types.binaryExpression('+', acc, part)
+            )
+          );
+        });
+
+        path.replaceWith(types.objectExpression(entries));
       },
     },
   };
 };
+
+function join(a, b) {
+  return a.length > 0
+    ? a.reduce(
+        (acc, item, index) =>
+          b.length > 0 && index < a.length - 1
+            ? acc.concat(item, b.shift())
+            : acc.concat(item),
+        []
+      )
+    : [b.shift()];
+}
--- test.js
+++ test.js
@@ -1,28 +1,66 @@
-import plugin from './index';
-import { transform } from '@babel/core';
+let plugin = require('./index');
+let { transform } = require('@babel/core');
 
 test('simple transform', () => {
-  let code = `
-    let style = css\`
-      font-size: 12px;
-      line-height: 1.5;
-      box-shadow:
-        1px 1px 0 red,
-        inset 2px 3px 0 green;
-      background-image: linear-gradient(to bottom, blue, red);
-    \`;
-  `.trim();
-  let expected = `
-    let style = {
-  fontSize: '12px',
-  lineHeight: '1.5',
-  boxShadow: '1px 1px 0 red, inset 2px 3px 0 green',
-  backgroundImage: 'linear-gradient(to bottom, blue, red)'
-};
-  `.trim();
+  let code = [
+    'let style = css`',
+    '  font-size: 12px;',
+    '  line-height: 1.5;',
+    '  box-shadow: 1px 1px 0 red, inset 2px 3px 0 green;',
+    '  background-image: linear-gradient(to bottom, blue, red);',
+    '`;',
+  ].join('\n');
+
+  let expected = [
+    'let style = {',
+    '  fontSize: "12px",',
+    '  lineHeight: "1.5",',
+    '  boxShadow: "1px 1px 0 red, inset 2px 3px 0 green",',
+    '  backgroundImage: "linear-gradient(to bottom, blue, red)"',
+    '};',
+  ].join('\n');
+
   let result = transform(code, {
     babelrc: false,
     plugins: [plugin],
   });
+
+  expect(result.code).toEqual(expected);
+});
+
+test('transform with expressions', () => {
+  let code = [
+    'let size = 12;',
+    'let lHeight = 1.5;',
+    'let color = "green";',
+    'let pTop = 13;',
+    'let style = css`',
+    '  font: normal ${size + 1}px/1.5 Helvetica;',
+    '  line-height: ${lHeight};',
+    '  padding-top: ${pTop}px;',
+    '  box-shadow: 1px 1px 0 red, inset 2px 3px 0 ${color};',
+    '  background-image: linear-gradient(to bottom, blue, red);',
+    '`;',
+  ].join('\n');
+
+  let expected = [
+    'let size = 12;',
+    'let lHeight = 1.5;',
+    'let color = "green";',
+    'let pTop = 13;',
+    'let style = {',
+    '  font: "normal " + (size + 1) + "px/1.5 Helvetica",',
+    '  lineHeight: lHeight,',
+    '  paddingTop: pTop + "px",',
+    '  boxShadow: "1px 1px 0 red, inset 2px 3px 0 " + color,',
+    '  backgroundImage: "linear-gradient(to bottom, blue, red)"',
+    '};',
+  ].join('\n');
+
+  let result = transform(code, {
+    babelrc: false,
+    plugins: [plugin],
+  });
+
   expect(result.code).toEqual(expected);
 });
